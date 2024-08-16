7b72d2a3064519adfad796c4960ae55335b684ce
shenn0n <shenn0n4412@gmail.com>
2024-08-16 14:11:42 +0300

    Initial Commit

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Александр Манжосов on 16.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
